95843808150bf3a613921934c74fcdcbb63e8f5e
khushiminda <khushipariminda@gmail.com>
2024-07-31 12:54:34 +0530

    chore:added a cards bad for features

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
-
+import Cards from '../components/Cards'
 function Home() {
   return (
     <Container>
@@ -10,6 +10,7 @@ function Home() {
       <Typography variant="body1">
         This is a project to ensure transaction anonymity in token-based CBDC transactions while maintaining financial system integrity and compliance with AML, CFT, and fraudulent mitigation compliances.
       </Typography>
+      <Cards/>
     </Container>
   );
 }
